949bfc5a4e36b99447ded74cacfd93ed32f3a560
farelamo <farizfahmi674@gmail.com>
2022-11-28 02:36:18 +0700

    Fix Emails

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,14 +4,14 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use App\Jobs\failedJobWorkshop;
+use App\Jobs\prepareJobWorkshop;
 
 class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new failedJobWorkshop())->everyMinute();
-        // $schedule->job(new failedJobWorkshop())->dailyAt('22:12')->timezone('Asia/Jakarta');
+        $schedule->job(new prepareJobWorkshop())->everyMinute();
+        // $schedule->job(new prepareJobWorkshop())->dailyAt('22:12')->timezone('Asia/Jakarta');
     }
 
     protected function commands()
