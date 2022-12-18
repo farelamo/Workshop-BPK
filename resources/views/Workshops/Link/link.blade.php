fa9b65b9c491deef23e38db362b9bb6b7c72094e
Muhammmad Asyroful <asyroful.muna12@gmail.com>
2022-12-18 09:53:46 +0700

    update halaman link

--- resources/views/Workshops/Link/link.blade.php
+++ resources/views/Workshops/Link/link.blade.php
@@ -1,141 +1,54 @@
+<div class="main-banner" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
+  <div class="container">
+    <div class="col-lg-12">
+      <h2>Link</h2>
+      <div class="upper my-3">
+        <button>
+          <div class="flex">
+            Create Link
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 6L12 18" stroke="#8B4BC4" stroke-width="2" stroke-linecap="round"/>
+              <path d="M18 12L6 12" stroke="#8B4BC4" stroke-width="2" stroke-linecap="round"/>
+            </svg>
 
-<!DOCTYPE html>
-<html lang="en">
-
-  <head>
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-
-    <title>Forum Workshop</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/index.css">
-    <link rel="stylesheet" href="assets/css/animated.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
-
-  </head>
-
-<body style="background-color: rgba(139, 75, 196, 0.9)">
-
-  <!-- ***** Preloader Start ***** -->
-  <div id="js-preloader" class="js-preloader">
-    <div class="preloader-inner">
-      <span class="dot"></span>
-      <div class="dots">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </div>
-  </div>
-  <!-- ***** Preloader End ***** -->
-
-  <!-- ***** Header Area Start ***** -->
-  <header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <nav class="main-nav">
-            <!-- ***** Logo Start ***** -->
-            <a href="index.html" class="logo">
-              <img src="assets/images/logo (1).png" alt="Chain App Dev">
-            </a>
-            <!-- ***** Logo End ***** -->
-            <!-- ***** Menu Start ***** -->
-            <ul class="nav">
-              <li><a href="#">Home</a></li>
-              <li><a href="listWorkshop.html">List Workshop</a></li>
-              <li><a href="createWorkshop.html">Create Workshop</a></li>
-              <li>
-                <ul id="dropdown">
-                  <li>
-                    <div class="dropbtn">
-                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <circle cx="12" cy="10" r="3" stroke="white" stroke-width="2" stroke-linecap="round"/>
-                      <circle cx="12" cy="12" r="9" stroke="white" stroke-width="2"/>
-                      <path d="M17.7805 18.8264C17.9076 18.7566 17.9678 18.6055 17.914 18.4708C17.5284 17.5045 16.7856 16.6534 15.7814 16.0332C14.6966 15.3632 13.3674 15 12 15C10.6326 15 9.30341 15.3632 8.21858 16.0332C7.21444 16.6534 6.4716 17.5045 6.08598 18.4708C6.03223 18.6055 6.09236 18.7566 6.21948 18.8264C9.81971 20.803 14.1803 20.803 17.7805 18.8264Z" fill="white"/>
-                      </svg>
-                      Hi, James!
-                      <svg width="14" height="11" viewBox="0 0 14 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M7.00002 11L0.0718145 0.5L13.9282 0.500001L7.00002 11Z" fill="white"/>
-                      </svg>
-                    </div>
-                    <ul>
-                      <li><a href="laporanPembicara.html"><span>Laporan sebagai Pembicara</span></a></li>
-                      <li><a href="laporanPeserta.html"><span>Laporan sebagai Peserta</span></a></li>
-                      <li><a href="#"><span>Logout</span></a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            <a class='menu-trigger'>
-                <span>Menu</span>
-            </a>
-            <!-- ***** Menu End ***** -->
-          </nav>
+          </div>
+        </button>
+        <div class="search">
+          <input type="address" name="address" class="align-self-end" placeholder="Search" autocomplete="on" required>
         </div>
       </div>
-    </div>
-  </header>
-  <!-- ***** Header Area End ***** -->
-
-  <div class="main-banner" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
-    <div class="container">
-      <div class="col-lg-12">
-        <h2>Link</h2>
-        <div class="upper my-3">
-          <button>
-            <div class="flex">
-              Create Link
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12 6L12 18" stroke="#8B4BC4" stroke-width="2" stroke-linecap="round"/>
-                <path d="M18 12L6 12" stroke="#8B4BC4" stroke-width="2" stroke-linecap="round"/>
-              </svg>
-
-            </div>
-          </button>
-          <div class="search">
-            <input type="address" name="address" class="align-self-end" placeholder="Search" autocomplete="on" required>
+      <div class="tabel first-service">
+        <div class="row head">
+          <div class="col-3">
+            <p>Sesi</p>
+          </div>
+          <div class="col-6">
+            <p>Link</p>
+          </div>
+          <div class="col-3">
+            <p>Aksi</p>
           </div>
         </div>
-        <div class="tabel first-service">
-          <div class="row head">
-            <div class="col-3">
-              <p>Sesi</p>
-            </div>
-            <div class="col-6">
-              <p>Link</p>
-            </div>
-            <div class="col-3">
-              <p>Aksi</p>
-            </div>
+        <hr>
+        <div class="row py-3">
+          <div class="col-3 judul">
+            <p>1</p>
           </div>
-          <hr>
-          <div class="row py-3">
-            <div class="col-3 judul">
-              <p>1</p>
-            </div>
-            <div class="col-6 tanggal">
-              <p>www.loremipsumdotsilerametdolorloremipsum.com</p>
-            </div>
-            <div class="col-3 d-flex justify-content-center">
-              <div class="d-flex flex-row">
+          <div class="col-6 tanggal">
+            <p>www.loremipsumdotsilerametdolorloremipsum.com</p>
+          </div>
+          <div class="col-3 d-flex justify-content-center">
+            <div class="d-flex flex-row">
+              <div style="cursor: pointer;" data-bs-toggle="modal" data-bs-target="#editWorkshop">
                 <div class="p-2 text-center me-1" style="background-color: rgba(151, 93, 202, 1); border-radius: 5px;">
+                  <!-- edit -->
                   <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M13.0863 7.91344L14.2498 6.75L14.2498 6.74997C14.3294 6.67034 14.3693 6.63052 14.4013 6.59526C15.0947 5.83244 15.0947 4.66756 14.4013 3.90474C14.3693 3.86949 14.3294 3.82967 14.2498 3.75005L14.2498 3.75C14.1701 3.67035 14.1303 3.63052 14.095 3.59847C13.3322 2.90508 12.1673 2.90508 11.4045 3.59847C11.3693 3.63052 11.3294 3.67035 11.2498 3.75L10.0688 4.93102C10.7821 6.17617 11.8241 7.21011 13.0863 7.91344ZM8.61405 6.38572L3.85615 11.1436C3.43109 11.5687 3.21856 11.7812 3.07883 12.0423C2.93909 12.3034 2.88015 12.5981 2.76226 13.1876L2.39686 15.0146C2.33034 15.3472 2.29708 15.5135 2.39168 15.6081C2.48629 15.7027 2.6526 15.6694 2.98521 15.6029L4.81219 15.2375L4.8122 15.2375C5.40164 15.1196 5.69637 15.0607 5.95746 14.9209C6.21856 14.7812 6.43109 14.5687 6.85614 14.1436L6.85614 14.1436L6.85615 14.1436L11.6278 9.37193C10.4169 8.60369 9.38944 7.58318 8.61405 6.38572Z" fill="white"/>
                   </svg>
                 </div>
+              </div>
+              <!-- delete -->
+              <div style="cursor: pointer;">
                 <div class="p-2 text-center" style="background-color: rgba(151, 93, 202, 1); border-radius: 5px;">
                   <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M14.875 4.25H2.125V6.375C2.9074 6.375 3.54167 7.00926 3.54167 7.79167V9.91667C3.54167 11.7464 3.54167 12.6613 3.92738 13.3466C4.19659 13.8248 4.59184 14.2201 5.0701 14.4893C5.75533 14.875 6.67022 14.875 8.5 14.875C10.3298 14.875 11.2447 14.875 11.9299 14.4893C12.4082 14.2201 12.8034 13.8248 13.0726 13.3466C13.4583 12.6613 13.4583 11.7464 13.4583 9.91667V7.79167C13.4583 7.00926 14.0926 6.375 14.875 6.375V4.25ZM7.72917 7.79167C7.72917 7.23938 7.28145 6.79167 6.72917 6.79167C6.17688 6.79167 5.72917 7.23938 5.72917 7.79167V11.3333C5.72917 11.8856 6.17688 12.3333 6.72917 12.3333C7.28145 12.3333 7.72917 11.8856 7.72917 11.3333V7.79167ZM11.2708 7.79167C11.2708 7.23938 10.8231 6.79167 10.2708 6.79167C9.71855 6.79167 9.27083 7.23938 9.27083 7.79167V11.3333C9.27083 11.8856 9.71855 12.3333 10.2708 12.3333C10.8231 12.3333 11.2708 11.8856 11.2708 11.3333V7.79167Z" fill="white"/>
@@ -145,162 +58,35 @@
               </div>
             </div>
           </div>
-          <hr>
-          <div class="pagination mt-3" >
-            <a href="#">&laquo;</a>
-            <a href="#">1</a>
-            <a href="#" class="active">2</a>
-            <p>...</p>
-            <a href="#">9</a>
-            <a href="#">10</a>
-            <a href="#">&raquo;</a>
-          </div>
+        </div>
+        <hr>
+        <div class="pagination mt-3" >
+          <a href="#">&laquo;</a>
+          <a href="#">1</a>
+          <a href="#" class="active">2</a>
+          <p>...</p>
+          <a href="#">9</a>
+          <a href="#">10</a>
+          <a href="#">&raquo;</a>
         </div>
       </div>
     </div>
   </div>
-
-  <footer id="newsletter">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 offset-lg-2">
-          <div class="section-heading">
-            <h4>Subscribe Our Newsletter</h4>
-          </div>
+</div>
+<div class="modal fade" id="editWorkshop" tabindex="-1" role="dialog" aria-labelledby="editWorkshop" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content p-4">
+        <div class="popupHeaderReg">
+          <span class="header_title">Edit Link Workshop</span>
+          <hr>
         </div>
-        <div class="col-lg-6 offset-lg-3">
-          <form id="search" action="#" method="GET">
-            <div class="row">
-              <div class="col-lg-6 col-sm-6">
-                <fieldset>
-                  <input type="address" name="address" class="email" placeholder="Email Address..." autocomplete="on" required>
-                </fieldset>
-              </div>
-              <div class="col-lg-6 col-sm-6">
-                <fieldset>
-                  <button type="submit" class="main-button">Subscribe Now <i class="fa fa-angle-right"></i></button>
-                </fieldset>
-              </div>
-            </div>
-          </form>
+        <div class="modal-body popupBody">
+          Link
+          <input class="mb-2" type="text">
+          <hr>
+          <button class="btn-primary" type="submit">Selesaikan Aksi</button>
+          <p class="pt-2" style="text-align: center; color: rgb(153, 151, 151); font-weight: 300;">*pastikan aksi yang anda lakukan baik dan benar</p>
         </div>
       </div>
-      <div class="row">
-        <div class="col-lg-3">
-          <div class="footer-widget">
-            <h4>About Us</h4>
-            <ul>
-              <li><a href="#">Lorem Ipsum</a></li>
-              <li><a href="#">Lorem Ipsum</a></li>
-              <li><a href="#">Lorem Ipsum</a></li>
-              <li><a href="#">Lorem Ipsum</a></li>
-              <li><a href="#">Lorem Ipsum</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-lg-3">
-          <div class="footer-widget">
-            <h4>List Workshop</h4>
-            <ul>
-              <li><a href="#">Lorem Ipsum</a></li>
-              <li><a href="#">Lorem Ipsum</a></li>
-              <li><a href="#">Lorem Ipsum</a></li>
-              <li><a href="#">Lorem Ipsum</a></li>
-              <li><a href="#">Lorem Ipsum</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-lg-3">
-          <div class="footer-widget">
-            <h4>Contact</h4>
-            <ul>
-              <li><a href="#">Lorem Ipsum</a></li>
-              <li><a href="#">Lorem Ipsum</a></li>
-              <li><a href="#">Lorem Ipsum</a></li>
-              <li><a href="#">Lorem Ipsum</a></li>
-              <li><a href="#">Lorem Ipsum</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-lg-3">
-          <div class="footer-widget">
-            <div class="row">
-              <div class="col-lg-3">
-                <a href="">
-                  <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M14.2268 25.5801H2.33587C1.57886 25.5801 0.965576 24.9536 0.965576 24.181V1.63049C0.965576 0.857689 1.57895 0.231445 2.33587 0.231445H24.423C25.1797 0.231445 25.7932 0.857689 25.7932 1.63049V24.181C25.7932 24.9537 25.1796 25.5801 24.423 25.5801H18.0963V15.7637H21.3234L21.8067 11.9381H18.0963V9.49566C18.0963 8.38806 18.3975 7.63326 19.9531 7.63326L21.9373 7.63237V4.2107C21.5941 4.16408 20.4163 4.05992 19.046 4.05992C16.1853 4.05992 14.2268 5.84275 14.2268 9.11679V11.9381H10.9913V15.7637H14.2268V25.5801V25.5801Z" fill="white"/>
-                  </svg>
-                </a>
-              </div>
-              <div class="col-lg-3">
-                <a href="">
-                  <svg width="25" height="22" viewBox="0 0 25 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M25 2.84444C24.0877 3.2686 23.1055 3.55543 22.0752 3.6836C23.1274 3.02448 23.9347 1.97934 24.315 0.73434C23.3299 1.34464 22.2413 1.78863 21.0784 2.02664C20.1516 0.990664 18.8269 0.34375 17.3609 0.34375C14.5498 0.34375 12.2692 2.73153 12.2692 5.67621C12.2692 6.09427 12.3129 6.50011 12.4004 6.8907C8.16697 6.66795 4.41449 4.54717 1.90069 1.31717C1.46205 2.10751 1.2114 3.02448 1.2114 4.00095C1.2114 5.85015 2.11054 7.48269 3.47746 8.43933C2.6439 8.41339 1.85697 8.1708 1.16914 7.77411V7.83971C1.16914 10.4243 2.92516 12.5802 5.25679 13.0684C4.82981 13.1935 4.37951 13.2561 3.91464 13.2561C3.58676 13.2561 3.26616 13.2241 2.95576 13.1615C3.60425 15.2792 5.48413 16.8218 7.71376 16.8629C5.97086 18.2941 3.77329 19.147 1.38773 19.147C0.976779 19.147 0.570199 19.1226 0.172363 19.0737C2.42677 20.5842 5.10524 21.4676 7.98044 21.4676C17.3507 21.4676 22.473 13.3431 22.473 6.29567C22.473 6.06375 22.4687 5.83184 22.4599 5.6045C23.4552 4.85231 24.3194 3.91398 25 2.84444" fill="white"/>
-                  </svg>
-                </a>
-              </div>
-              <div class="col-lg-3">
-                <a href="">
-                  <svg width="26" height="23" viewBox="0 0 26 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M25.1945 5.47185C25.0865 7.97154 23.396 11.3886 20.1348 15.7245C16.7598 20.2584 13.9 22.5238 11.5629 22.5238C10.1177 22.5238 8.89293 21.1455 7.89297 18.3798C7.22293 15.8424 6.55726 13.3049 5.88867 10.775C5.14417 8.00781 4.34712 6.62497 3.4946 6.62497C3.30921 6.62497 2.66252 7.02849 1.54723 7.83703L0.379395 6.27889C1.6027 5.16808 2.8085 4.05274 3.99823 2.94043C5.62737 1.48052 6.85214 0.714288 7.66962 0.634189C9.59656 0.440743 10.7848 1.80544 11.233 4.72376C11.7103 7.87482 12.0461 9.83346 12.2315 10.6012C12.7891 13.2127 13.3993 14.5155 14.0679 14.5155C14.5861 14.5155 15.3657 13.6722 16.4051 11.984C17.4444 10.2929 17.9977 9.00829 18.0736 8.12267C18.2225 6.66276 17.6663 5.93733 16.4051 5.93733C15.8109 5.93733 15.1978 6.0673 14.5686 6.33934C15.7949 2.23012 18.1305 0.232184 21.5815 0.348554C24.1376 0.422607 25.3448 2.1334 25.1945 5.47185" fill="white"/>
-                  </svg>
-                </a>
-              </div>
-              <div class="col-lg-3">
-                <a href="">
-                  <svg width="26" height="19" viewBox="0 0 26 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M24.1786 1.79575C24.9226 2.57038 25.1655 4.32945 25.1655 4.32945C25.1655 4.32945 25.4138 6.39566 25.4138 8.46107V10.398C25.4138 12.4643 25.1655 14.5297 25.1655 14.5297C25.1655 14.5297 24.9226 16.2887 24.1786 17.0634C23.3279 17.9797 22.3847 18.0744 21.8515 18.1279C21.7929 18.1338 21.7392 18.1392 21.6912 18.1451C18.2169 18.4036 13 18.4116 13 18.4116C13 18.4116 6.5448 18.351 4.5586 18.1547C4.46449 18.1366 4.35041 18.1226 4.22123 18.1066C3.59208 18.029 2.6046 17.9073 1.82058 17.0634C1.07653 16.2887 0.834458 14.5297 0.834458 14.5297C0.834458 14.5297 0.586182 12.4643 0.586182 10.398V8.46107C0.586182 6.39566 0.834458 4.32945 0.834458 4.32945C0.834458 4.32945 1.07653 2.57038 1.82058 1.79575C2.67314 0.878089 3.61728 0.784432 4.15047 0.731539C4.20828 0.725805 4.26126 0.720549 4.30877 0.714775C7.78308 0.456299 12.9945 0.456299 12.9945 0.456299H13.0054C13.0054 0.456299 18.2169 0.456299 21.6912 0.714775C21.7387 0.720558 21.7917 0.725821 21.8496 0.731565C22.3823 0.784462 23.3269 0.878238 24.1786 1.79575ZM10.4358 5.57202L10.4366 12.7447L17.1439 9.17074L10.4358 5.57202Z" fill="white"/>
-                  </svg>
-                </a>
-              </div>
-            </div>
-            <p class="mt-3">Jl. Pusdiklat BPK RI Jl. Raya Kalibata, RT.6/RW.7, Kalibata, Jakarta Selatan, DKI Jakarta, Daerah Khusus Ibukota Jakarta 12750</p>
-          </div>
-        </div>
-      </div>
-      <hr>
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="footer-widget">
-            <div class="logo">
-              <img src="assets/images/forum.png" width="100%" alt="">
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="footer-widget mt-5">
-            <img src="assets/images/logo (1).png" alt="">
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="row">
-            <div class="col-lg-6">
-              <div class="footer-widget">
-                <p><a href="#">Terms of Service</a></p>
-              </div>
-            </div>
-            <div class="col-lg-6">
-              <div class="footer-widget">
-                <p><a href="#">Privacy Policy</a></p>
-              </div>
-            </div>
-          </div>
-          <div class="copyright-text">
-            <p>Copyright © 2022 BPK Corpo. All rights reserved. </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-
-  <!-- Scripts -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/js/owl-carousel.js"></script>
-  <script src="assets/js/animation.js"></script>
-  <script src="assets/js/imagesloaded.js"></script>
-  <script src="assets/js/popup.js"></script>
-  <script src="assets/js/custom.js"></script>
-</body>
-</html>
+  </div>
+</div>
